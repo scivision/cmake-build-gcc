50426a22deb355ad3a70427bf81ff375eab354e8
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-31 14:20:46 -0400

    use gcc 11.2 release

--- gcc.cmake
+++ gcc.cmake
@@ -7,10 +7,12 @@ set(gcc_args
 --enable-languages=c,c++,fortran
 )
 
-set(gcc_url https://bigsearcher.com/mirrors/gcc/snapshots/LATEST-12/gcc-12-20220327.tar.xz)
+set(gcc_url https://bigsearcher.com/mirrors/gcc/releases/gcc-11.2.0/gcc-11.2.0.tar.xz)
+set(gcc_sha512 d53a0a966230895c54f01aea38696f818817b505f1e2bfa65e508753fcd01b2aedb4a61434f41f3a2ddbbd9f41384b96153c684ded3f0fa97c82758d9de5c7cf)
 
 ExternalProject_Add(gcc_compiler
 URL ${gcc_url}
+URL_HASH SHA512=${gcc_sha512}
 CONFIGURE_COMMAND <SOURCE_DIR>/configure ${gcc_args} CFLAGS=${CMAKE_C_FLAGS} LDFLAGS=${LDFLAGS}
 BUILD_COMMAND ${MAKE_EXECUTABLE} -j
 INSTALL_COMMAND ${MAKE_EXECUTABLE} -j install
