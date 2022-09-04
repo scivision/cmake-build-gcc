56a4314abfadd55b7a65e57b8f2c9d01c4bc0ab0
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-04 19:50:10 -0400

    disable docs

--- ExtProj.cmake
+++ ExtProj.cmake
@@ -1,3 +1,4 @@
+# This is for autotools based projects
 include(ExternalProject)
 
 set(extproj_args)
@@ -15,13 +16,14 @@ list(INSERT args 0 "--prefix=${CMAKE_INSTALL_PREFIX}")
 
 ExternalProject_Add(${name}
 URL ${url}
-CONFIGURE_COMMAND <SOURCE_DIR>/configure ${args} CFLAGS=${CMAKE_C_FLAGS} LDFLAGS=${LDFLAGS}
-BUILD_COMMAND ${MAKE_EXECUTABLE} -j${NCPU}
+CONFIGURE_COMMAND <SOURCE_DIR>/configure ${args} CFLAGS=${CMAKE_C_FLAGS} LDFLAGS=${LDFLAGS} MAKEINFO=true
+BUILD_COMMAND ${MAKE_EXECUTABLE} -j${NCPU} MAKEINFO=true
 INSTALL_COMMAND ${MAKE_EXECUTABLE} -j${NCPU} install
 TEST_COMMAND ""
 TLS_VERIFY true
 ${extproj_args}
 DEPENDS ${dep}
 )
+# MAKEINFO=true disables docs using makeinfo to save time and avoid missing makeinfo error
 
 endfunction(extproj)
